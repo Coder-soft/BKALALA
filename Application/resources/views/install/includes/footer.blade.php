0dd3ab6c65b709384e7e69f569d47ff56392cdc3
Coder-soft <136147222+Coder-soft@users.noreply.github.com>
2024-07-22 16:41:31 +0500

    Polled

--- Application/resources/views/install/includes/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/js/app.js') }}"></script>
